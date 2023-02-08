1bed19eac3a4639db9a75f230a6de916301bea9b
Joshua Jerome <joshua.jerome@gmail.com>
2023-02-08 10:31:01 -0800

    sample text working now?

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ class Main {
     private static Database database = new Database();
     public static void main(String[] args) {
         database.clearDatabase();
-        database.addNameFile("sample.txt");
+        database.addNameFile("sample2.txt");
         database.printDatabase();
 
-        System.out.println("\n");
-        database.add("Asher");
-        database.printDatabase();
+        // System.out.println("\n");
+        // database.add("Asher");
+        // database.printDatabase();
 
-        database.clearDatabase();
+        // database.clearDatabase();
     }
 }
