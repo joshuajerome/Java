849bb3e29b3729caa2980b307f1df1f110ffff06
Joshua Jerome <joshua.jerome@gmail.com>
2023-02-13 18:56:47 -0800

    Created split method and helper functions

--- src/Event.java
+++ src/Event.java
@@ -50,7 +50,54 @@ public class Event extends Database{
         System.out.println(sb.toString());
     }
 
+    private double sumList(List<Double> list) {
+        double sum = 0;
+        for (double i : list) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    private List<Double> expected(Double totalCost, int k) {
+        List<Double> expectedValues = new ArrayList<>();
+        double avgValue = totalCost/k;
+        double sumCheck = 0.0;
+        for (int i = 0; i < k; i++) {
+            expectedValues.add(avgValue);
+            sumCheck += avgValue;
+        }
+        if (sumCheck != totalCost) expectedValues.set(0, avgValue + (totalCost - sumCheck));
+        return expectedValues;
+    }
     
+    /** BRANCH MANAGEMENT
+     *  1) git checkout -b "name of branch" origin/main
+     *  - check what branch i'm on: git branch -a
+     *  - see differences: git diff "File name"
+     *  2) Make changes
+     *  3) git add
+     *  4) git commit -m "message"
+     *  5) git push origin "branch name"
+     *  6) create pull request
+     *  7) Get approval
+     *  8) create merge request
+     *  9) Then locally...
+     * 10) git checkout main
+     * 11) git fetch
+     * 12) git rebase 
+     * 13) git branch -D "branch name"
+     */
+
+    public List<List<Double>> split(List<Double> allCosts, int k) {
+        List<List<Double>> subArrays = new ArrayList<>();
+        
+        int n = allCosts.size();
+        double allCostsSum = sumList(allCosts);
+        List<Double> kExpected = expected(allCostsSum, k);
+        List<Double> kActual;
+
+        return subArrays;
+    }
 
 
 
