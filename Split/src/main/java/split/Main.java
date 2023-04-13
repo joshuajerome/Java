9758f6178e6971a6e55d22c429ac4576fbf38ad8
Joshua Jerome <joshua.jerome@gmail.com>
2023-04-13 16:51:59 -0700

    testing calculator

--- Split/src/main/java/split/Main.java
+++ Split/src/main/java/split/Main.java
@@ -34,8 +34,8 @@ class Main {
         System.out.println("Test Case 1:");
         Double array1[] = new Double[] {7.0,11.0,13.0,9.0,41.0,37.0,23.0,29.0,43.0}; // 53.25 split by 4
         totalCost = Arrays.asList(array1);
-        System.out.println(c.printSubArrays(totalCost,9));
-        System.out.println(c.printSubArraysOLD(totalCost,9));
+        System.out.println(c.printSubArrays(totalCost,4));
+        System.out.println(c.printSubArraysOLD(totalCost,4));
 
         System.out.println("Test Case 2:");
         Double array2[] = new Double[] {5.0,7.0,11.0,14.0,22.0,39.0,50.0}; // 49.33 split by 3
